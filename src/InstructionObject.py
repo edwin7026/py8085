88a1ab49a583f6662183e270a0c33ec6ab1f4232
edwin7026 <edwin7026@gmail.com>
2022-02-18 12:38:44 +0530

    instrObject handling labels and address of instruciton

--- src/InstructionObject.py
+++ src/InstructionObject.py
@@ -6,8 +6,10 @@ class instrObject:
     '''
     Holds all parameters consituting an instuction
     '''
-    def __init__(self, 
-                func, 
+    def __init__(self,
+                instr_addr = None,
+                label = None,
+                func = None, 
                 rst = None, 
                 psw = None, 
                 addr16 = None, 
@@ -19,24 +21,27 @@ class instrObject:
         Initializer
 
         Arguments:
-        func:   (string) The functional part of the opcode
-        rst:    (int 0-7) RST type
-        psw:    (boolean) Pop/Push from/to PSW
-        addr16: (string) 16-bit address for the immediate addressing instructions
-        opr_reg: (string) Operand register for arithmetic instructions
-        rd:     (string) Destination register
-        rs:     (string) Source register
-        imm:    (string) Immediate value in the instruction
+        instr_addr: (int, hex) Address of the instruction in the memory
+        label:      (string) Mnemonic label if any
+        func:       (string) The functional part of the opcode
+        rst:        (int 0-7) RST type
+        psw:        (boolean) Pop/Push from/to PSW
+        addr16:     (string) 16-bit address for the immediate addressing instructions
+        opr_reg:    (string) Operand register for arithmetic instructions
+        rd:         (string) Destination register
+        rs:         (string) Source register
+        imm:        (string) Immediate value in the instruction
         '''
+        self.instr_addr = instr_addr
+        self.label = label
         self.func = func
-        self.rst = rst
+        self.rst = int(rst)
         self.psw = psw
-        self.addr16 = addr16
+        self.addr16 = int(addr16, 16)
         self.opr_reg = opr_reg
         self.rd = rd
         self.rs = rs
-        self.imm = imm
-    
+        self.imm = int(imm, 16)
 
     
 
