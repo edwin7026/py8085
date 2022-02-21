b5e7417fba6ed6fc837e74e165afd3622bbe34ac
edwin7026 <edwin7026@gmail.com>
2022-02-21 21:26:46 +0530

    For tests and debugging

---
+++ py8085/main.py
@@ -0,0 +1,10 @@
+# Name: Edwin Joy
+# email: edwin7026@gmail.com
+
+import utils
+
+# For tests
+if __name__ == '__main__':
+    
+    utils.process_file('../tests/test1.S')
+    utils.print_all_mnemonics()
