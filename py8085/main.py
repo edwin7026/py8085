32c0e155b66666cb0ccf4fe05d2b82c19446655a
edwin7026 <edwin7026@gmail.com>
2022-03-03 18:40:38 +0530

    Assember test

--- py8085/main.py
+++ py8085/main.py
@@ -1,10 +1,18 @@
 # Name: Edwin Joy
 # email: edwin7026@gmail.com
 
-import utils
+from py8085.Assembler import assembler
+from py8085 import utils
+#from Assembler import assembler
 
 # For tests
 if __name__ == '__main__':
     
     utils.process_file('../tests/test1.S')
-    utils.print_all_mnemonics()
+    #utils.print_all_mnemonics()
+    
+    instr_list = utils.get_all_instr_obj()
+    for obj in instr_list:
+        #print(obj)
+        assm = assembler(obj)
+        print(assm.get_hex_code())
