5b95428c4f4dde6514257f6a035d37cb685b4998
edwin7026 <edwin7026@gmail.com>
2022-03-03 18:39:50 +0530

    Package changes

--- py8085/__init__.py
+++ py8085/__init__.py
@@ -1,7 +1,5 @@
-# See LICENSE for details
+'''Top-level package for py8085'''
 
-'''Top-level package for py8085.'''
-
-__author__ = '''Edwin Joy'''
+__author__ = 'Edwin Joy'
 __email__ = 'edwin7026@gmail.com'
 __version__ = '0.0.1'
